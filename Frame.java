82c41f3e196a5cee549e95e9b59b32d9c2ef8e27
Amit-Dey <amitdey9020@gmail.com>
2021-09-05 13:09:17 +0600

    File Added

---
+++ Frame.java
@@ -0,0 +1,13 @@
+package com.CarMoving;
+
+import javax.swing.*;
+
+public class Frame extends JFrame {
+    public static void main(String[] args) {
+        Frame frame = new Frame();
+        frame.setSize(628,390);
+        frame.add(new Board());
+        frame.setVisible(true);
+    }
+
+}
